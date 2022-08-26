7b03127a635028c5b6c2c1397c6ad97e7af3d0b2
zachary sibson <zac.sibson@gmail.com>
2022-08-25 21:01:00 -0600

    added table and data to person.sql

---
+++ artist.sql
@@ -0,0 +1,5 @@
+CREATE TABLE person (Person ID int, 
+Name varchar(100), 
+Age int, 
+Height float, 
+favorit_color text);
---
+++ person.sql
@@ -0,0 +1,21 @@
+CREATE TABLE person (
+ID int PRIMARY KEY, 
+Name varchar(100), 
+Age int, 
+Height float, 
+favorite_color text);
+
+INSERT INTO person (Name, Age, Height, favorite_color)
+VALUES ('Zac', '25', '5.9', 'blue')
+
+INSERT INTO person (Name, Age, Height, favorite_color)
+VALUES ('Brooke', '22', '5.9', 'lavender')
+
+INSERT INTO person (Name, Age, Height, favorite_color)
+VALUES ('Luca', '4', '2', 'Brown')
+
+INSERT INTO person (Name, Age, Height, favorite_color)
+VALUES ('Kelsey', '22', '5.7', 'Yellow')
+
+INSERT INTO person (Name, Age, Height, favorite_color)
+VALUES ('Justin', '24', '6', '');
